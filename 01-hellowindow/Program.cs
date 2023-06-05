d834044b44c63d3e4bbed3bb693fad1df50c2204
David Perfors <dnperfors@gmail.com>
2023-06-05 10:58:17 +0200

    Add HelloWindow program, from both LearnOpenGL.com and Silk.NET tutorials.

---
+++ 01-hellowindow/Program.cs
@@ -0,0 +1,50 @@
+﻿using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.OpenGL;
+using Silk.NET.Windowing;
+
+var options = WindowOptions.Default with
+{
+    Size = new Vector2D<int>(800, 600),
+    Title = "Learn OpenGL"
+};
+
+IWindow window = Window.Create(options);
+GL? gl = null;
+
+window.Load += OnLoad;
+window.Update += OnUpdate;
+window.Render += OnRender;
+
+window.Run();
+
+void OnLoad()
+{
+    var input = window.CreateInput();
+    gl = window.CreateOpenGL();
+
+    foreach(var keyboard in input.Keyboards)
+    {
+        keyboard.KeyDown += OnKeyDown;
+    }
+}
+
+void OnKeyDown(IKeyboard keyboard, Key key, int keyCode)
+{
+    if (key == Key.Escape)
+    {
+        window.Close();
+    }
+}
+
+void OnUpdate(double dt)
+{
+}
+
+void OnRender(double dt)
+{
+    if (gl is null) return;
+
+    gl.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    gl.Clear(ClearBufferMask.ColorBufferBit);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This repository contains my .NET version of [LearnOpenGL](https://learnopengl.com) using Silk.NET.
